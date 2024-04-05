facc258facc0965229b272856e91356114a06a03
Paul Tindall <paul@wellstandhealth.com>
2024-04-04 21:03:03 -0500

    chore: more build number updates

--- scripts/update_build_numbers.sh
+++ scripts/update_build_numbers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 awk "{gsub(/DEV-0/,$(cat keycloak-build-number.txt)); print}" Dockerfile.tmpl > Dockerfile
+
+awk "{gsub(/DEV-0/,$(cat keycloak-build-number.txt)); print}" pom.xml > pom.xml.tmp \
+&& mv pom.xml.tmp pom.xml
